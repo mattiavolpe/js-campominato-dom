b6f1e2fbad18acbb441b17e1f2fefb8f9fe67867
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-20 12:04:44 +0100

    ADD bombs generation and check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
 /*
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
 
-Bonus
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella:
+se il numero è presente nella lista dei numeri generati
+abbiamo calpestato una bomba
+la cella si colora di rosso e la partita termina.
+Altrimenti
+la cella cliccata si colora di azzurro
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Consigli del giorno: :baby-yoda:
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Pensiamo prima al codice senza fare funzioni, poi facendo refactoring implementiamo la funzione corrispondente.
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Eventuali validazioni e controlli possiamo farli anche in un secondo momento.
 */
 
 // SELECT THE CONTAINER FROM THE DOM
@@ -30,27 +44,33 @@ playButton.addEventListener("click", function() {
   // CREATES THE GRID
   createNewGrid(cellsNumber, containerElement);
 
+  // CREATES THE BOMBS
+  const bombs = createBombs(cellsNumber);
+  
+  // CREATE A VARIABLE TO TRACK THE SCORE
+  let score = 0;
+
   // GETS THE INSERTED CELLS
   const insertedCells = document.querySelectorAll(".cell");
 
   // LISTENS FOR CLICK ON CELLS TO CHANGE THEIR BACKGROUND COLOR
-  listenForSelection(insertedCells);
+  listenForSelection(insertedCells, bombs, score);
 });
 
 // <---------- FUNCTIONS ---------->
 
 // FUNCTION TO EXTRACT THE NUMBER OF CELLS NEEDED
 function setCellsNumber(selectorElement) {
-  let cells = 100;
+  let cells = 49;
   switch (selectorElement.value) {
     case "medium":
       cells = 81;
       break;
     case "easy":
-      cells = 49;
+      cells = 100;
       break;
     default:
-      cells = 100;
+      cells = 49;
   }
   return cells;
 }
@@ -76,15 +96,44 @@ function createNewGrid(totalCells, container) {
   }
 }
 
+// FUNCTION TO CREATE THE BOMBS
+function createBombs(totalCells) {
+  // CREATE AN EMPTY ARRAY TO CONTAIN THE 16 BOMBS
+  const bombsArray = [];
+
+  // GENERATE A RANDOM NUMBER BETWEEN 1 AND THE NUMBER OF CELLS IN THE GRID, 16 TIMES
+  for (let i = 0; i < 16; i++) {
+    let bomb = Math.floor(Math.random() * totalCells) + 1;
+
+    // CONTROLS IF THE GENERATED NUMBER IS ALREADY IN THE BOMBS ARRAY
+    while (bombsArray.includes(bomb)) {
+      bomb = Math.floor(Math.random() * totalCells) + 1;
+    }
+    bombsArray.push(bomb);
+  }
+
+  // SORT THE BOMBS IN THE ARRAY
+  bombsArray.sort((a, b) => a - b);
+  return bombsArray;
+}
+
 // FUNCTION TO CHANGE THE BACKGROUND OF A CELL WHEN CLICKED
-function listenForSelection(cellsInTheGrid) {
+function listenForSelection(cellsInTheGrid, bombsArray, score) {
 
   // ADDS A "CLICK" EVENT LISTENER TO EVERY CELL IN THE GRID
   for (let i = 0; i < cellsInTheGrid.length; i++) {
     const currentCell = cellsInTheGrid[i];
     currentCell.addEventListener("click", function() {
-      this.classList.add("selected");
-      console.log(Number(this.innerText));
+      if (this.className.includes("selected")) {
+        return;
+      } else if (bombsArray.includes(Number(this.innerText))) {
+        alert(`HAI PERSO. IL TUO PUNTEGGIO E' ${score}`);
+        location.reload();
+      } else {
+        this.classList.add("selected");
+        console.log(Number(this.innerText));
+        score++;
+      }
     });
   }
 }
